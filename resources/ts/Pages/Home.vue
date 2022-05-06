b75279104ac5e0cb6271440ac5b9b8b6e9a01a39
Phillip Mwaniki <codezeroprivate@gmail.com>
2022-05-06 15:43:43 +0300

    Added Home component

---
+++ resources/ts/Pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Howdy</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+    name: "Home",
+    setup() {
+        //
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
