3e98ea6c35a1be6a45689a01e4368a8261e68334
Phillip Mwaniki <codezeroprivate@gmail.com>
2022-05-09 12:05:54 +0300

    added ziggy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <title>Abubu</title>
+    @routes
     @production
         @php
             $manifest = json_decode(file_get_contents(public_path('dist/manifest.json')), true);
